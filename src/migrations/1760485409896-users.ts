651780b930a6e23f07201b5951a619a2a88b4ec8
thisme8 <stutiupreti8@gmail.com>
2025-10-16 05:48:31 +0545

    Add if not exixts to user migration

--- src/migrations/1760485409896-users.ts
+++ src/migrations/1760485409896-users.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class Users1760485409896 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-        `CREATE TABLE "users" (
+        `CREATE TABLE IF NOT EXISTS "users" (
         "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
         "name" VARCHAR(255) NOT NULL,
         "username" VARCHAR(255) UNIQUE NOT NULL,
