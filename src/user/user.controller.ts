eb0ebc6f807ca5a41a0ca57ca0a40361a3c097af
thisme8 <stutiupreti8@gmail.com>
2025-10-17 12:52:33 +0545

    Resolve the PR comment in US-06

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,26 +7,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly usersService: UserService) {}
 
-  @Post('signup')
+  @Post('/')
   async create(@Body() createUserDto: RegisterUserDto) {
     const user = await this.usersService.create(createUserDto);
-    return { message: 'User registered successfully. Please verify your email.', user };
+    return { message: 'User registered successfully. Please verify your email.', data: { user } };
   }
 
-  @Get('get')
+  @Get('/')
   async getAllUsers() {
-    return this.usersService.getAllUsers();
+    const users = await this.usersService.getAllUsers();
+    return { message: 'Here are all the users.', data: { users } };
   }
 
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+    const user = await this.usersService.update(id, updateUserDto);
+    return { message: 'The user has been updated successfully', data: { user } };
   }
 
   @Delete(':id')
   async remove(@Param('id') id: string) {
-    return this.usersService.remove(id);
+    return (this.usersService.remove(id), { message: 'The user has been deleted successfully' });
   }
 }
-
-
