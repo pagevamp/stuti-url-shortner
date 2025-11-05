e1bc5f482aaeb31a069e61bb9da11c1ef38b5c2c
thisme8 <stutiupreti8@gmail.com>
2025-11-05 12:42:27 +0545

    Add relevant comments

--- src/url-analytics/dto/analytics-filter.dto.ts
+++ src/url-analytics/dto/analytics-filter.dto.ts
@@ -2,12 +2,12 @@ import { Transform } from 'class-transformer';
 import { IsDateString, IsISO31661Alpha2, IsOptional, IsString } from 'class-validator';
 
 export class AnalyticsFilterDto {
-  @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true })
+  @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true }) //to make the query date compatible with the date in database
   @IsOptional()
   @IsDateString()
   start_date: Date;
 
-  @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true })
+  @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true }) //to make the query date compatible with the date in database
   @IsOptional()
   @IsDateString()
   end_date: Date;
@@ -26,6 +26,6 @@ export class AnalyticsFilterDto {
 
   @IsString()
   @IsOptional()
-  @IsISO31661Alpha2()
+  @IsISO31661Alpha2() // to validate country codes with two character in UpperCase
   country: string;
 }
