fcdf708654d5ffb8c692eea08e1c26a7b07bac61
thisme8 <stutiupreti8@gmail.com>
2025-11-06 12:50:52 +0545

    Make chnages with guard imports

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -13,11 +13,10 @@ import { UrlAnalyticsModule } from 'url-analytics/url-analytics.module';
 import { UserModule } from 'user/user.module';
 import { MailModule } from 'utils/mail.module';
 import { LogModule } from 'log/log.module';
-import { CoreModule } from 'core/core.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Url, User, Log, UrlAnalytics, CoreModule]),
+    TypeOrmModule.forFeature([Url, User, Log, UrlAnalytics]),
     BullModule.registerQueue({ name: 'url_analytics' }),
     UrlAnalyticsModule,
     UserModule,
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,10 +6,9 @@ import { UserService } from './user.service';
 import { HashService } from './hash.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
-import { CoreModule } from 'core/core.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), JwtModule, ConfigModule, CoreModule],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule, ConfigModule],
   controllers: [UserController],
   providers: [UserService, HashService],
   exports: [UserService, HashService],
