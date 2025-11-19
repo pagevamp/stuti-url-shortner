1a28ca2e42161bd6d190868d0305851d1af6a80d
thisme8 <stutiupreti8@gmail.com>
2025-11-19 09:29:41 +0545

    Fix(deploy) : Add CORS compatibility to the backend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
