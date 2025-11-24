07268f606dcd1f6cef692f9196756991d405dbd9
thisme8 <stutiupreti8@gmail.com>
2025-11-25 05:39:57 +0545

    Fix(auth-token) : Make same-site configuration as none

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,8 @@ export class AuthController {
 
     res.cookie('accessToken', access_token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
+      sameSite: 'none',
       path: '/',
       maxAge: 48 * 60 * 60 * 1000, // 2 days
     });
