2ce29c0891202d826773c3cc9601d1ab2cdbeee7
thisme8 <stutiupreti8@gmail.com>
2025-11-25 12:02:43 +0545

    Fix(deploy) : Remove the path as /

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,9 +16,9 @@ export class AuthController {
 
     res.cookie('accessToken', access_token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'lax',
-      path: '/',
+      secure: true,
+      sameSite: 'none',
+      // path: '/',
       maxAge: 48 * 60 * 60 * 1000, // 2 days
     });
     return { message: 'Logged in successfully' };
